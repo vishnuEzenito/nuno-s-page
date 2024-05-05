530c9218efe5eb85e40316679185bb35979f720e
vishnu <vishnu@vishnus-MacBook-Pro.local>
2024-05-05 13:00:00 +0530

    recent fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Footer from "@/components/Footer";
-import {Category, Hero, Benifits, Process } from "@/components/contact";
+import {Hero } from "@/components/contact";
 import NavBar from "@/components/Navbar";
 import Image from "next/image";
 import { Box } from '@mui/material';
