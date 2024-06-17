fc11dd57949a78e9febabf1cf7ff35043d98ed0b
Akshat Mittal <akshatmittal2506@gmail.com>
2024-06-18 01:39:58 +0530

    fix: amc issues

--- src/components/contact/Hero/index.tsx
+++ src/components/contact/Hero/index.tsx
@@ -1,105 +1,204 @@
 // components/HeroCarousel.js
 
-import React,{useEffect,useState} from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { Button, Typography, Grid,Box,ButtonBase,Paper } from '@mui/material';
-import { TextField,Alert,AlertTitle } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import {
+	Button,
+	Typography,
+	Grid,
+	Box,
+	ButtonBase,
+	Paper,
+} from "@mui/material";
+import { TextField, Alert, AlertTitle } from "@mui/material";
 
-import { Toolbar } from '@mui/material';
+import { Toolbar } from "@mui/material";
 import Link from "next/link";
 
-
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
-import '../../../fonts/fonts.css'
-
-
-
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
+import "../../../fonts/fonts.css";
 
 const Hero = () => {
+	const theme = useTheme();
+	const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+	const isMediumScreen = useMediaQuery(theme.breakpoints.between("md", "lg"));
+	const isLargeScreen = useMediaQuery(theme.breakpoints.between("lg", "xl"));
+	const isExtraLargeScreen = useMediaQuery(theme.breakpoints.up("xl"));
+	const [loading, setLoading] = useState(true);
 
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-  const isMediumScreen = useMediaQuery(theme.breakpoints.between('md', 'lg'));
-  const isLargeScreen = useMediaQuery(theme.breakpoints.between('lg', 'xl'));
-  const isExtraLargeScreen = useMediaQuery(theme.breakpoints.up('xl'));
-  const [loading, setLoading] = useState(true);
+	return (
+		<>
+			<link
+				href="https://cdn.jsdelivr.net/npm/@vetixy/circular-std@1.0.0/dist/index.min.css"
+				rel="stylesheet"
+			></link>
+			<Box
+				sx={{
+					mb: "4rem",
+					mt: "4rem",
+					display: "flex",
+					textAlign: "center",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+					pb: "3rem",
+					width: "100%",
+					height: "auto",
+					px: isSmallScreen
+						? "1.25rem"
+						: isMediumScreen
+						? "4.5rem"
+						: isLargeScreen
+						? "4.5rem"
+						: isExtraLargeScreen
+						? "5.5rem"
+						: "4.5rem",
+				}}
+			>
+				<Typography
+					variant="h1"
+					sx={{
+						textAlign: "center",
+						fontFamily: "classicsans",
+						fontWeight: "bold",
+						color: "#5B5B5B",
+						fontSize: isSmallScreen
+							? "28px"
+							: isMediumScreen
+							? "45px"
+							: isLargeScreen
+							? "50px"
+							: isExtraLargeScreen
+							? "50px"
+							: "45rem",
+					}}
+				>
+					Get in touch with us!
+				</Typography>
+				<Typography
+					variant="subtitle2"
+					sx={{
+						textAlign: "center",
+						whiteSpace: isMediumScreen ? "balance" : "break-spaces",
+						fontFamily: "classicsans",
+						fontWeight: "light",
+						color: "#8A8A89",
+						fontSize: isSmallScreen ? "14px" : "20px",
+						mt: "0.5rem",
+					}}
+				>
+					Contact us for support or collabration
+				</Typography>
+				<Box
+					sx={{
+						gap: "1rem",
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						width: "50%",
+						mt: "2rem",
+					}}
+				>
+					<TextField
+						id="name"
+						placeholder="Name"
+						name="name"
+						required
+						type="name"
+						sx={{
+							background: "#F4F4F4",
+							".css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input":
+								{ borderRadius: "12px" },
+							".MuiOutlinedInput-input": { borderRadius: "12px" },
+							borderRadius: "12px",
+							width: "100%",
+						}}
+					/>
+					<TextField
+						name="email"
+						placeholder="Email"
+						required
+						id="email"
+						type="email"
+						sx={{
+							background: "#F4F4F4",
+							border: "hidden",
+							borderRadius: "12px",
+							width: "100%",
+						}}
+					/>
 
+					<TextField
+						name="Org"
+						id="org"
+						placeholder="organisation"
+						type="org"
+						required
+						sx={{
+							background: "#F4F4F4",
+							borderRadius: "12px",
+							width: "100%",
+						}}
+					/>
 
-  return (<>
-
-    <link href="https://cdn.jsdelivr.net/npm/@vetixy/circular-std@1.0.0/dist/index.min.css" rel="stylesheet"></link>
-    <Box sx={{ mb:'4rem',mt:'4rem', display: 'flex',textAlign:'center', flexDirection: 'column', justifyContent: 'center', alignItems:'center', pb:'3rem',width:'100%', height: 'auto',px: isSmallScreen ? '1.25rem' : (isMediumScreen ? '4.5rem' : (isLargeScreen ? '4.5rem' : (isExtraLargeScreen ? '5.5rem' : '4.5rem')))}}>
-      <Typography variant="h1" sx={{textAlign:'center',fontFamily: 'classicsans', fontWeight: "bold", color: '#5B5B5B', fontSize: isSmallScreen ? '28px' : (isMediumScreen ? '45px' : (isLargeScreen ? '50px' : (isExtraLargeScreen ? '50px' : '45rem'))) }}>
-      
-      Get in touch with us!
-      </Typography>
-          <Typography variant="subtitle2" sx={{textAlign:'center',whiteSpace: isMediumScreen?'balance':'break-spaces',fontFamily: 'classicsans', fontWeight: 'light', color: '#8A8A89', fontSize: isSmallScreen ? '14px' : '20px', mt:"0.5rem", }}>
-          Contact us to for support or colabration
-          </Typography>
-          <Box sx={{gap:'1rem',display:'flex',flexDirection:'column',justifyContent:'center',width:'50%',mt:'2rem'}}>
-      <TextField
-          id="name"
-          placeholder="Name"
-          name="name"
-          required
-          type='name'
-          sx={{background:'#F4F4F4','.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input':{borderRadius:'12px'},'.MuiOutlinedInput-input':{borderRadius:'12px'},borderRadius:'12px',width:'100%',}}
-
-        />
-      <TextField
-          name="email"
-          placeholder="Email"
-          required
-          id="email"
-          type='email'
-          sx={{background:'#F4F4F4',border:'hidden',borderRadius:'12px',width:'100%'}}
-
-        />
-
-      <TextField name="Org" id="org" placeholder="organisation" type='org' required sx={{background:'#F4F4F4',borderRadius:'12px',width:'100%'}}
-
-/>
-
-      <TextField
-          id="message"
-          name="message"
-          label=""
-          multiline
-          rows={4}
-          placeholder="Message"
-          defaultValue=""
-          sx={{background:'#F4F4F4',borderRadius:'12px',width:'100%'}}
-        />
-  
-
-    </Box>
-          <Box sx={{display:"flex",flexDirection:'row',gap:'2rem'}}>
-          <ButtonBase  sx={{
-                mt:'2rem',
-                borderRadius: '12px',
-                width:'auto'}}>
-        <Paper sx={{
-                borderRadius: '12px',
-                background:'#B34038',
-                width:'auto',
-                px:'2rem',
-                py:'0.3rem',
-                minWidth:'200px',
-                }}>
-                  
-                <Typography variant="body2" sx={{ whiteSpace:'break-spaces',textAlign:'center',fontFamily: 'CircularStd, sans-serif', fontWeight: 100, color:'#fff',fontSize:isSmallScreen ? '14px' : '20px',py:'0.5rem',}}>
-                Send
-                </Typography>
-              </Paper>
-              </ButtonBase>
-              </Box>
-
-
-              </Box>
-    </>);
+					<TextField
+						id="message"
+						name="message"
+						label=""
+						multiline
+						rows={4}
+						placeholder="Message"
+						defaultValue=""
+						sx={{
+							background: "#F4F4F4",
+							borderRadius: "12px",
+							width: "100%",
+						}}
+					/>
+				</Box>
+				<Box
+					sx={{ display: "flex", flexDirection: "row", gap: "2rem" }}
+				>
+					<ButtonBase
+						sx={{
+							mt: "2rem",
+							borderRadius: "12px",
+							width: "auto",
+						}}
+					>
+						<Paper
+							sx={{
+								borderRadius: "12px",
+								background: "#B34038",
+								width: "auto",
+								px: "2rem",
+								py: "0.3rem",
+								minWidth: "200px",
+							}}
+						>
+							<Typography
+								variant="body2"
+								sx={{
+									whiteSpace: "break-spaces",
+									textAlign: "center",
+									fontFamily: "CircularStd, sans-serif",
+									fontWeight: 100,
+									color: "#fff",
+									fontSize: isSmallScreen ? "14px" : "20px",
+									py: "0.5rem",
+								}}
+							>
+								Send
+							</Typography>
+						</Paper>
+					</ButtonBase>
+				</Box>
+			</Box>
+		</>
+	);
 };
 
 export default Hero;
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,171 +1,175 @@
 const HomeData = {
+	hero: {
+		title: {
+			txt1: "A global B2B marketplace for",
+			txt3: "Sustainable Procurement",
+			img1: "/Zero Emission.png",
+			img2: "/Benefit.png",
+			img3: "/verified.png",
+			img4: "/Link.png",
+			img5: "/renewable.png",
+			img6: "/bio.png",
+			img7: "/footprint.png",
+			img8: "/certified.png",
+		},
 
-    hero: {
-        title: {
-            txt1: "A global B2B marketplace for​",
-            txt3: "Sustainable Procurement",
-            img1:'/Zero Emission.png',
-            img2:'/Benefit.png',
-            img3:'/verified.png', 
-            img4:'/Link.png',
-            img5:'/renewable.png',
-            img6:'/bio.png',
-            img7:'/footprint.png', 
-            img8:'/certified.png',
-        },
+		buttonText: "Know more",
+		deskTopImgUrl: "/heroimg.png",
+		deskTopImgUrl2: "/bgimg2.jpg",
 
-        buttonText: 'Know more',
-        deskTopImgUrl: '/heroimg.png',
-        deskTopImgUrl2: '/bgimg2.jpg',
+		mobileImpUrl: "/hero-img2.png",
+	},
 
-        mobileImpUrl: '/hero-img2.png'
-    },
+	about: [
+		{ img: "/about1.png" },
+		{ img: "/about2.png" },
+		{ img: "/about3.png" },
+		{ img: "/about4.png" },
+		{ img: "/about5.png" },
+		{ img: "/about6.png" },
+	],
 
-    about: [
-        
-        {img:'/about1.png'},
-        {img:'/about2.png'},       
-        {img:'/about3.png'},
-        {img:'/about4.png'},
-        {img:'/about5.png'},
-        {img:'/about6.png'},
-        
-],
-
-    canvas: {
-        list: null
-    },
-    AssessmentData: {
-        list: null
-    },
-    personalblogs: {
-        list: [
-            {
-                id: 1,
-                imgUrl: "/assets/Cheatsheet.png",
-                url: "",
-                tag:"Nuno's Post",
-                text: "Top 5 and od aosd techniques to learn job negotiations with case studies",
-                description:"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
-            },
-            {
-                id: 2,
-                imgUrl: "/assets/canvas1.png",
-                url: "",
-                tag:"Nuno's Post",
-                text: "Top 5 techniques to learn job negotiations with case studies",
-                description:"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
-            },
-            {
-                id: 3,
-                imgUrl: "/assets/image 22.png",
-                url: "",
-                tag:"Nuno's Post",
-                text: "Top 5 techniques to learn job negotiations with case studies",
-                description:"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
-            },
-            {
-                id: 4,
-                imgUrl: "/assets/canvas1.png",
-                url: "",
-                tag:"Nuno's Post",
-                text: "Top 5 techniques to learn job negotiations with case studies",
-                description:"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
-            },
-            {
-                id: 5,
-                imgUrl: "/assets/canvas1.png",
-                url: "",
-                tag:"Nuno's Post",
-                text: "Top 5 techniques to learn job negotiations with case studies",
-                description:"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
-            },
-        ]
-    },
-    blogapi: {
-        records:null
-        
-    },
-    cta: {
-        section1: {
-            title: {
-                txt1: "Find the right products to meet the sustainability goals of your business​",
-            },
-            bgImg:'/ctabg.png',
-            buttonText: "Go to store",
-        },
-        section2: {
-            deskTopImgUrl: "/sellonallmatter.png",
-            mobileImgUrl:"/sellonallmatter.png",
-            title: "Sell on All Mattr",
-            subtitle: {
-                txt1: "Partner with us and take your planet-positive products to the world!​",
-            },
-            buttonText: "Connect Now"
-        }
-    },
-    whyChooseUs: {
-        title: "WHY CHOOSE US",
-        list: [
-            {
-                id: 1,
-                imgUrl: "/assets/step1.svg",
-                url: "",
-                text: "Negotiate",
-                content:'for maximum impact: adopt empowering mindsets,  prepare with the Negotiation Canvas,  and apply, impact, and learn'
-            },
-            {
-                id: 2,
-                imgUrl: "/assets/step2.svg",
-                url: "",
-                text: "Connect",
-                content:'through solid bridges:ever-improving working relationships plus effective and efficient communication​'
-            },
-            {
-                id: 3,
-                imgUrl: "/assets/step3.svg",
-                url: "",
-                text: "Create",
-                content:'high-impact deals by satisfying all parties’ interests, exploring creative options, and persuading with legitimacy.'
-            },
-            {
-                id: 4,
-                imgUrl: "/assets/step4.svg",
-                url: "",
-                text: "Choose",
-                content:'to either implement solid commitments or walk away to pursue good alternatives.​'
-
-            },
-        ]
-    },
-    benifits: {
-        list: [
-            {
-                id: 1,
-                url: "",
-                text: "Increase visibility and get more sales",
-                content:'Get business from buyers around the world who are specifically looking for sustainable products'
-            },
-            {
-                id: 2,
-                url: "",
-                text: "Higher order volumes",
-                content:'Get bigger order volumes as our focus is to bring wholesale orders from global buyers'
-            },
-            {
-                id: 3,
-                url: "",
-                text: "Fulfilment services",
-                content:'We shall take care of the logistics while you can focus on delivering quality products and growing your business.'
-            },
-        ]
-    },
-    books : {
-        records: null,    
-    },
-   
-}
-
-export {
-    HomeData
+	canvas: {
+		list: null,
+	},
+	AssessmentData: {
+		list: null,
+	},
+	personalblogs: {
+		list: [
+			{
+				id: 1,
+				imgUrl: "/assets/Cheatsheet.png",
+				url: "",
+				tag: "Nuno's Post",
+				text: "Top 5 and od aosd techniques to learn job negotiations with case studies",
+				description:
+					"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
+			},
+			{
+				id: 2,
+				imgUrl: "/assets/canvas1.png",
+				url: "",
+				tag: "Nuno's Post",
+				text: "Top 5 techniques to learn job negotiations with case studies",
+				description:
+					"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
+			},
+			{
+				id: 3,
+				imgUrl: "/assets/image 22.png",
+				url: "",
+				tag: "Nuno's Post",
+				text: "Top 5 techniques to learn job negotiations with case studies",
+				description:
+					"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
+			},
+			{
+				id: 4,
+				imgUrl: "/assets/canvas1.png",
+				url: "",
+				tag: "Nuno's Post",
+				text: "Top 5 techniques to learn job negotiations with case studies",
+				description:
+					"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
+			},
+			{
+				id: 5,
+				imgUrl: "/assets/canvas1.png",
+				url: "",
+				tag: "Nuno's Post",
+				text: "Top 5 techniques to learn job negotiations with case studies",
+				description:
+					"for maximum impact: Adopt empowering mindsets Prepare with the Negotiation Canvas Apply, impact, and learn",
+			},
+		],
+	},
+	blogapi: {
+		records: null,
+	},
+	cta: {
+		section1: {
+			title: {
+				txt1: "Find the right products to meet the sustainability goals of your business",
+			},
+			bgImg: "/ctabg.png",
+			buttonText: "Go to store",
+		},
+		section2: {
+			deskTopImgUrl: "/sellonallmatter.png",
+			mobileImgUrl: "/sellonallmatter.png",
+			title: "Sell on All Mattr",
+			subtitle: {
+				txt1: "Partner with us and take your planet-positive products to the world!",
+			},
+			buttonText: "Connect Now",
+		},
+	},
+	whyChooseUs: {
+		title: "WHY CHOOSE US",
+		list: [
+			{
+				id: 1,
+				imgUrl: "/assets/step1.svg",
+				url: "",
+				text: "Negotiate",
+				content:
+					"for maximum impact: adopt empowering mindsets,  prepare with the Negotiation Canvas,  and apply, impact, and learn",
+			},
+			{
+				id: 2,
+				imgUrl: "/assets/step2.svg",
+				url: "",
+				text: "Connect",
+				content:
+					"through solid bridges: ever-improving working relationships plus effective and efficient communication​",
+			},
+			{
+				id: 3,
+				imgUrl: "/assets/step3.svg",
+				url: "",
+				text: "Create",
+				content:
+					"high-impact deals by satisfying all parties' interests, exploring creative options, and persuading with legitimacy.",
+			},
+			{
+				id: 4,
+				imgUrl: "/assets/step4.svg",
+				url: "",
+				text: "Choose",
+				content:
+					"to either implement solid commitments or walk away to pursue good alternatives.",
+			},
+		],
+	},
+	benifits: {
+		list: [
+			{
+				id: 1,
+				url: "",
+				text: "Increase visibility and get more sales",
+				content:
+					"Get business from buyers around the world who are specifically looking for sustainable products",
+			},
+			{
+				id: 2,
+				url: "",
+				text: "Higher order volumes",
+				content:
+					"Get bigger order volumes as our focus is to bring wholesale orders from global buyers",
+			},
+			{
+				id: 3,
+				url: "",
+				text: "Fulfilment services",
+				content:
+					"We shall take care of the logistics while you can focus on delivering quality products and growing your business.",
+			},
+		],
+	},
+	books: {
+		records: null,
+	},
 };
+
+export { HomeData };
